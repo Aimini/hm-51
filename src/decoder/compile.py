c37b12989c3fd754c652d8ebd04a56888e3a9d7d
Aimini <2026084033@qq.com>
2019-12-21 22:03:12 +0800

    refactor: unsing enum replace hard encoded state

--- src/decoder/compile.py
+++ src/decoder/compile.py
@@ -3,93 +3,158 @@ import os
 import sys
 import tokenize
 import json
+import enum
 
 
+class cstate(enum.Enum):
+    ERROR = enum.auto()
+    START = enum.auto()
+    NAME = enum.auto()  # you get a name, but, is it a parameterized control labal, or jump label, or...
+    CONTROL_LINEEND = enum.auto()  # it's a control label, and now it's in end of the line
+    CONTROL = enum.auto()   # control label too, not at end of the line
+    JUMP_MARK = enum.auto()  # jump name
+    PARAMETER_LIST_BEGIN = enum.auto()  # check wheather a parameter at next token
+    NEXT_NAME = enum.auto()  # name after parameterized control name list
+    CONVERT = enum.auto()  # you accumulated all the control names in this line, you should convert it to control signal now
 
-def next_state(state, t):
-    """
-    according current state and t transfer to next state.
-    see transfer diagram at directory
-    state:
-        current state
-    t:
-        token
-    """
-    ttype = t.type
-    tstr = t.string
+    # sub state of PARAMETER_LIST_BEGIN
+    PAR_SEPERATE = enum.auto()
+    GET_ONE_PARAMETER = enum.auto()
+    PARAMETER_LIST_END = enum.auto()
 
-    def is_name():
-        return ttype == tokenize.NAME
 
-    is_name = ttype == tokenize.NAME
-    is_number = ttype == tokenize.NUMBER
-    is_newline = ttype == tokenize.NEWLINE or ttype == tokenize.NL
-    is_op = ttype == tokenize.OP
-    def check_op(s): return is_op and tstr == s
+    def next(self, token):
+        """
+        according current state and t transfer to next state.
+        see transfer diagram at directory
+        state:
+            current state
+        t:
+            token
+        """
+        ttype = token.type
+        tstr = token.string
+        if ttype in (tokenize.COMMENT, tokenize.ENCODING):
+            if not self.isnoconsume():
+                return self
+        is_name = ttype == tokenize.NAME
+        is_number = ttype == tokenize.NUMBER
+        is_lineend = ttype in (tokenize.ENDMARKER, tokenize.NEWLINE) or ttype == tokenize.NL
+        is_op = ttype == tokenize.OP
 
-    if t.type in (tokenize.COMMENT, tokenize.ENCODING) and state not in (3, 10):
-        return state
+        
+        scls = type(self)
+        def check_op(s): return is_op and tstr == s
 
-    elif state == 1:
-        if is_name:
-            return 2
-        if is_newline:
-            return 1
-        else:
-            return 0
-    elif state == 2:
-        if is_op:
-            if tstr == ',':
-                return 4
-            elif tstr == ':':
-                return 5
-            elif tstr == '(':
-                return 6
+        if self == scls.START:
+            if is_name:
+                return scls.NAME
+            if is_lineend:
+                return scls.START
             else:
-                return 0
-        elif is_newline:
-            return 3
-        else:
-            return 0
-    elif state == 3:
-        return 10
-    elif state == 4:
-        if is_name:
-            return 2
-        else:
-            return 0
-    elif state == 5:
-        if is_name:
-            return 2
-        elif is_newline:
-            return 1
-        else:
-            return 0
-    elif state == 6:
-        if is_name or is_number:
-            return 6001
-        elif check_op(')'):
-            return 6002
-    elif state == 10:
-        return 1
-    ####### sub state of S6
-    elif state == 6001:
-        if check_op(','):
-            return 6
-        elif check_op(')'):
-            return 6002
-        else:
-            return 0
-    elif state == 6002:
-        if is_newline:
-            return 10
-        elif check_op(','):
-            return 1
-        else:
-            return 0
-    else:
-        return 0
+                return scls.ERROR
+        elif self == scls.NAME:
+            if is_op:
+                if tstr == ',':
+                    return scls.CONTROL
+                elif tstr == ':':
+                    return scls.JUMP_MARK
+                elif tstr == '(':
+                    return scls.PARAMETER_LIST_BEGIN
+                else:
+                    return scls.ERROR
+            elif is_lineend:
+                return scls.CONTROL_LINEEND
+            else:
+                return scls.ERROR
+        elif self == scls.CONTROL_LINEEND:
+            return scls.CONVERT
+        elif self == scls.CONTROL:
+            if is_name:
+                return scls.NAME
+            else:
+                return scls.ERROR
+        elif self == scls.JUMP_MARK:
+            if is_name:
+                return scls.NAME
+            elif is_lineend:
+                return scls.START
+            else:
+                return scls.ERROR
+        elif self == scls.PARAMETER_LIST_BEGIN:
+            if is_name or is_number:
+                return scls.GET_ONE_PARAMETER
+            elif check_op(')'):
+                return scls.PARAMETER_LIST_END
+        elif self == scls.NEXT_NAME:
+            if is_name:
+                return scls.NAME
+            else:
+                return scls.ERROR
+        elif self == scls.CONVERT:
+            return scls.START
 
+        ####### sub state of S6
+        elif self == scls.PAR_SEPERATE:
+            if check_op(')'):
+                return scls.PARAMETER_LIST_END
+            elif is_name or is_number:
+                return scls.GET_ONE_PARAMETER
+            else:
+                return scls.ERROR
+        elif self == scls.GET_ONE_PARAMETER:
+            if check_op(','):
+                return scls.PAR_SEPERATE
+            elif check_op(')'):
+                return scls.PARAMETER_LIST_END
+            else:
+                return scls.ERROR
+        elif self == scls.PARAMETER_LIST_END:
+            if is_lineend:
+                return scls.CONVERT
+            elif check_op(','):
+                return scls.NEXT_NAME
+            else:
+                return scls.ERROR
+        else:
+            return scls.ERROR
+
+    def isnoconsume(self):
+        scls = type(self)
+        return self in (scls.CONVERT, scls.CONTROL_LINEEND)
+
+    def iserror(self):
+        pass
+
+
+class statement_accumulator:
+    """
+    control accumulator pack tokens to a control label
+    parameterized control label or jump label.
+    """
+
+    def __init__(self):
+        self.parameters_owner = None
+        self.parameters = []
+
+        self.mark_lines = []
+        self.marks_per_line = []
+        pass
+
+    def add(self, state, appendtokens):
+        if state in (cstate.CONTROL_LINEEND, cstate.CONTROL):
+            self.marks_per_line.append(appendtokens[-2])
+        elif state == cstate.PARAMETER_LIST_BEGIN:
+            self.parameters_owner = appendtokens[-2]
+        elif state == cstate.GET_ONE_PARAMETER:
+            self.parameters.append(appendtokens[-1])
+        elif state == cstate.PARAMETER_LIST_END:
+            self.marks_per_line.append([self.parameters_owner, self.parameters])
+            self.parameters = []
+        elif state == cstate.CONVERT:
+            self.mark_lines.append(self.marks_per_line)
+            self.marks_per_line = []
+        
 
 def compile_ds(readline, write):
     """
@@ -105,16 +170,23 @@ def compile_ds(readline, write):
     # each line's tokens list
     tokens_lines = [[]]
     scanned_tokens = []
-    state = 1
-    previous_state = 1  # debug
+
+    state = cstate.START
+    previous_state = state  # debug
+    sa = statement_accumulator()
     for t in tks:
-        if t.type == tokenize.ENDMARKER:
-            break
 
         previous_state = state
-        state = next_state(state, t)
         while True:
-            if state == 0:
+            state = state.next(t)        
+            scanned_tokens.append(t)
+            if t.type == tokenize.NL or t.type == tokenize.NEWLINE:
+                tokens_lines.append([])
+            else:
+                tokens_lines[-1].append(t)
+
+            sa.add(state, scanned_tokens)
+            if state == cstate.ERROR:
                 estr = json.dumps(t.string).strip('"')
                 e = SyntaxError()
                 e.msg = "unexpect token \"{}\"".format(estr)
@@ -124,17 +196,23 @@ def compile_ds(readline, write):
                 e.filename = ""
                 raise e
             # it does not consume any token
-            elif state in (10, 3):
-                state = next_state(state, t)
+            elif state.isnoconsume():
+                continue
             else:
                 break
 
-        scanned_tokens.append(t)
-        if t.type == tokenize.NL or t.type == tokenize.NEWLINE:
-            tokens_lines.append([])
-        else:
-            tokens_lines[-1].append(t)
-
+    for i in sa.mark_lines:
+        for x in i:
+            if isinstance(x, list):
+                print(x[0].string,end = '')
+                print("(", end = '')
+                for y in x[1]:
+                    print(y.string, end=',')
+                print(")", end = '')
+            else:
+                print(x.string,end = '')
+            print('',end = ',')
+        print()
 
 def compile_ds_to_file(infile, outfile):
     """
