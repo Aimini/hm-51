945ef9a9e5304c26dd5de62f1357242ef3cdf743
Aimini <2026084033@qq.com>
2019-12-28 15:02:47 +0800

    feat: compile asm file to intel hex file

--- test/compile_all.py
+++ test/compile_all.py
@@ -1,13 +1,25 @@
 import subprocess
 import sys
 import pathlib
-def asmobj(strfilename, filedir, tempdir):
-    filename =  pathlib.Path(filedir) / strfilename
+"""
+usage:
+    compile_all <input_file> <output_dir>
+    input_file is 51 assembly file
+    ouput_dir is used to store ouput file,it's an intel hex format file, and it have the same name as input file.
+"""
+def asmobj(filenamestr, tempdirstr):
+    filename =  pathlib.Path(filenamestr)
+    tempdir = pathlib.Path(tempdirstr)
     filestem = filename.stem
-    outputfilename =  pathlib.Path(tempdir) / (filestem + ".obj")
-    subprocess.run(f"A51 {filename} OBJECT({outputfilename})")
+    objfile =  tempdir / (filestem + ".obj")
+    subprocess.run(f"A51 {filename} OBJECT({objfile})")
+    absfile =  tempdir / (filestem + ".abs")
+    subprocess.run(f"BL51 {objfile} TO {absfile}")
+    hexfile =  tempdir / (filestem + ".hex")
+    subprocess.run(f"OH51 {absfile}  HEXfile({hexfile})")
+    
 
 if len(sys.argv) < 2:
     print("[error] assembly filename missing")
 else:
-    asmobj(sys.argv[1], R"test\src", R"test\temp")
+    asmobj(sys.argv[1], sys.argv[2])
