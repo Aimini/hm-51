07fe2ae53693fd826a8966c9bde5ebedd1ac6846
Aimini <2026084033@qq.com>
2020-01-14 19:07:58 +0800

    feat: generate MOV direct, direct test.

---
+++ test/generate/85_MOV_d_d.py
@@ -0,0 +1,49 @@
+#########################################################
+# 2020-01-08 19:45:35
+# AI
+# ins: MOV direct, #immed.
+# how: write address of the iram and SFR into itself.
+#########################################################
+
+import __util as u
+from __asmconst import *
+
+p = u.create_test()
+
+def one(*vargs):
+    a = vargs[0]
+    b = vargs[1]
+    # swap value
+    return 
+
+p.iter_is(lambda  addr: atl.move(atl.D(addr), atl.I(addr)))
+
+# move to self
+#### IRAM 0,1,2,3...0x7E, 0x7F
+#### SFR  0x81, 0x82, 0x83, 0xA8, 0xB8, 0xD0, 0xE0, 0xF0,
+p(";;;;;;;;;;;;;;;;;;;;;; move to self")
+p.iter_is(lambda  addr: atl.move(atl.D(addr), atl.D(addr)))
+
+
+
+# mirror content in IRAM
+#### IRAM 0x7F, 0x7E, 0x7D ... 0x7D,0x7E,0x7F
+#### SFR  0x81, 0x82, 0x83, 0xA8, 0xB8, 0xD0, 0xE0, 0xF0,
+p(";;;;;;;;;;;;;;;;;;;;;; mirror iram")
+p.iterx_iram(lambda a,b: atl.move(atl.D(a), atl.I(b)))
+
+# some complicate
+#### IRAM 0x81, 0x82, 0x83, 0xA8, 0xB8, 0xD0, 0xE0, 0xF0,    0x77 ... 3,2,1,0
+#### SFR  0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 
+p(";;;;;;;;;;;;;;;;;;;;;; swap part sfr and iram")
+for i, v in enumerate(p.rsfr()):
+    #  move RAM[i] <- SFR[i]
+    p(atl.move(atl.D(i), atl.D(v)))
+    #  move SFR[i] <- RAM[0x40 + i]
+    p(atl.move(atl.D(v), atl.D(0x40 + i)))
+
+# mirror content in SFR
+#### IRAM 0x81, 0x82, 0x83, 0xA8, 0xB8, 0xD0, 0xE0, 0xF0, 0x77 ... 3,2,1,0
+#### SFR  0x47, 0x46, 0x45, 0x44, 0x44, 0x45, 0x46, 0x47, 
+p(";;;;;;;;;;;;;;;;;;;;;; swap sfr")
+p.iterx_sfr(lambda a,b: atl.move(atl.D(a), atl.D(b)))
--- test/generate/__util.py
+++ test/generate/__util.py
@@ -3,45 +3,75 @@ import sys
 import pathlib
 import io
 import atexit
+import itertools
 import __asmutil as atl
-
 SFR_MAP = {
-    0x81:2,#"SP"],
-    0x82:4,#"DPL"],
-    0x83:5,#"DPH"],
-    0xA8:6,#"IE"],
-    0xB8:7,#"IP"],
-    0xD0:3,#"PSW"],
-    0xE0:0,#"A"],
-    0xF0:1,#"B"],
+    0x81: 2,  # "SP"],
+    0x82: 4,  # "DPL"],
+    0x83: 5,  # "DPH"],
+    0xA8: 6,  # "IE"],
+    0xB8: 7,  # "IP"],
+    0xD0: 3,  # "PSW"],
+    0xE0: 0,  # "A"],
+    0xF0: 1,  # "B"],
 }
+
 
 class asm_test:
     def __init__(self, filename):
         self.filename = filename
         self.sio = io.StringIO()
+
         def store_file():
             with open(self.filename, "w") as fh:
-                fh.writelines(self.sio.getvalue())
+                
+                fh.write(self.sio.getvalue())
                 fh.write(atl.exit())
                 fh.write('\n')
                 fh.write("END\n")
-                
         atexit.register(store_file)
 
-    def __call__(self,* vargs, **kargs):
+    def __call__(self, * vargs, **kargs):
         """
         using as print, but write string to test file.
         """
-        print(*vargs, file = self.sio, **kargs)
-       
+        print(*vargs, file=self.sio, **kargs)
+
+    @staticmethod
+    def rdirect():
+        """
+        get range or direct
+        """
+        return range(0, 0x100)
+
+    @staticmethod
+    def riram():
+        """
+        get range or iram
+        """
+        return range(0, 0x80)
+
+    @staticmethod
+    def rsfr():
+        """
+        get range of SFR
+        """
+        return sorted(SFR_MAP.keys())
+
+    @staticmethod
+    def ris():
+        """
+        get range of iram and SFR
+        """
+        return itertools.chain(asm_test.riram(), asm_test.rsfr())
+
     def iter_direct(self, f):
         """
         iterate direct address  0 - 255
             f: function
                 call f(x), x is direct address
         """
-        for x in range(256):
+        for x in self.rdirect():
             self(f(x))
 
     def iter_iram(self, f):
@@ -49,18 +79,45 @@ class asm_test:
         iram address
         0 - 127
         """
-        for x in range(128):
+        for x in self.riram():
             self(f(x))
 
-
-    def iter_is(self,f):
+    def iter_is(self, f):
         """
         iterate iram and SFR in RF.
         """
         self.iter_iram(f)
-        for k in sorted(SFR_MAP.keys()):
+        for k in self.ris():
             self(f(k))
 
+    def iterx(self, iter_obj, f):
+        """
+        provide cross iterator a and b,
+        a iterates from front to back,
+        b iterates from back to front
+        """
+        l = len(iter_obj)
+        a = list(iter_obj)
+        for i, v in enumerate(a):
+            self(f(v, a[l - i - 1]))
+
+    def iterx_iram(self, f):
+        """
+        cross iter iram, see iterx in asm_test
+        """
+        self.iterx(self.riram(), f)
+
+    def iterx_sfr(self, f):
+        """
+        cross iter iram, see iterx in asm_test
+        """
+        self.iterx(self.rsfr(), f)
+
+    def iterx_is(self, f):
+        """
+        cross iter iram and SFR, see iterx in asm_test
+        """
+        self.iterx(self.ris(), f)
 
 
 def create_test():
@@ -90,8 +147,3 @@ def create_test():
     ofilepath = pathlib.Path(op.output_dir) / (pathlib.Path(sys.argv[0]).stem + ".A51")
 
     return asm_test(ofilepath)
-
-
-
-
-
