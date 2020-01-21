a7fd8946cee457285fe0aaa847e0be39c690a092
Aimini <2026084033@qq.com>
2020-01-21 23:48:15 +0800

    feat:0x06-0x07, INC @Ri, test passed.

---
+++ test/generate/06_07_INC_ri.py
@@ -0,0 +1,32 @@
+#########################################################
+# 2020-01-08 19:45:35
+# AI
+# ins: MOV direct, #immed.
+# how: write address of the iram and SFR into itself.
+#########################################################
+
+import __util as u
+import random
+from __asmconst import *
+
+p = u.create_test()
+def init_rs(rs):
+    return atl.move(SFR_PSW, atl.I(rs << 3))
+
+def iter_ri(rs,ri):
+    t = 20
+    ri = atl.RI(rs, ri)
+    addr =random.getrandbits(7)
+
+    p(atl.move(atl.D(ri.addr), atl.I(addr)))
+    start =random.getrandbits(8)
+
+    p(atl.move(atl.D(addr), atl.I(start)))
+    for x in range(t):
+        p("INC {}".format(ri))
+        p(atl.aste(atl.D(addr), atl.I((start + x + 1)%256)))
+    return ""
+
+
+
+p.iter_ri(init_rs, iter_ri)
