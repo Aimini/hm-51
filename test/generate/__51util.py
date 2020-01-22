d95e3067755295955ff9e49d3e121c535a498f9c
Aimini <2026084033@qq.com>
2020-01-22 22:02:07 +0800

    feat: add SIMRAM class to help simulate content  change in RAM.

---
+++ test/generate/__51util.py
@@ -0,0 +1,21 @@
+from __asmconst import SFR_A, SFR_PSW
+
+
+class SIMRAM():
+    def __init__(self):
+        self.RAM = [0 for _ in range(0x100)]
+
+    def __getitem__(self, k: int):
+        return self.RAM[k]
+
+    def __setitem__(self, k: int, v):
+        if k == SFR_PSW.x:
+            pf = self.RAM[SFR_PSW.x] & 1
+            self.RAM[SFR_PSW.x] = (v & 0xFE) | pf
+        else:
+            self.RAM[k] = v
+            
+        if k == SFR_A.x:
+            pf = bin(self.RAM[SFR_A.x]).count('1') & 1
+            self.RAM[SFR_PSW.x] &= 0xFE
+            self.RAM[SFR_PSW.x] |= pf
