a433087705b581d060347e53b412091f9319ef57
Aimini <2026084033@qq.com>
2020-01-24 19:21:45 +0800

    feat: 0x60, jZ offset. test passed.

---
+++ test/generate/60_JZ_o.py
@@ -0,0 +1,67 @@
+#########################################################
+# 2020-01-24 11:16:58
+# AI
+# ins: JZ offset
+#########################################################
+
+import __util as u
+import random
+from __asmconst import *
+from __numutil import numutil as ntl
+
+p = u.create_test()
+
+jump_limit =f"""
+; PC = 0
+MOV ACC, #0      ; SET　 A TO 0
+JZ JMP_LIMIT_127 ; JUMP TO  2 + 0x7F = 0x81
+; PC = 2
+NOP
+; PC = 3
+JMP_LIMIT_256:
+JZ JMP_SEG_START
+
+
+CSEG AT 0x81
+JMP_LIMIT_127:
+JZ JMP_LIMIT_256 ; JUMP TO 0x83 - 0x80 = 0x03
+; PC = 0x83
+
+JMP_SEG_START:
+"""
+p += jump_limit
+
+
+def creat_jump_link(p, jump_count, order):
+    jmpords = ntl.jl(jump_count)
+    start =random.choice(jmpords)
+    p += f"""
+    SJMP JMP_SEG_{order}_{start}
+    """
+    
+    for idx,next_idx in enumerate(jmpords):
+        p += f"JMP_SEG_{order}_{idx}:"
+        A = random.getrandbits(8)
+        p += atl.move(SFR_A,atl.I(A))
+        
+        if next_idx == start:
+            target = f"JMP_SEG_END_{order}"
+        else:
+            target = f"JMP_SEG_{order}_{next_idx}"
+
+        if A == 0:
+            p += f"JZ {target}"
+            p += atl.astl(I_00, I_00)
+        else:
+            jump_wrong = f"SEG_WRONG_{order}_{idx}"
+
+            p += f"JZ {jump_wrong}"
+            p += f"SJMP {target}"
+            p += f"{jump_wrong}:"
+            p += atl.astl(I_00, I_00)
+
+    p += f"JMP_SEG_END_{order}:"
+
+for x in range(500):
+    creat_jump_link(p, 8,x)
+
