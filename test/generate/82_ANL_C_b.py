f19df177d577aa63788c0c23cd380bda49f16853
Aimini <2026084033@qq.com>
2020-01-25 18:08:57 +0800

    feat: 0x82, ANL C, bit. test passed.

---
+++ test/generate/82_ANL_C_b.py
@@ -0,0 +1,35 @@
+#########################################################
+# 2020-01-25 18:07:34
+# AI
+# ins: ANL C, bit
+#########################################################
+
+import __util as u
+import random
+from __asmconst import *
+from __numutil import numutil as ntl
+from __51util import SIMRAM
+
+p = u.create_test()
+ram = SIMRAM()
+
+
+def init(addr, p):
+    v = random.getrandbits(8)
+    p += atl.move(atl.D(addr),atl.I(v))
+    ram[addr] = v
+    return ""
+
+def test(addr, idx, p):
+    B = ram.bit(addr, idx)
+    C = ram.bit(SFR_PSW.x, 7)
+    ram.set_bit(SFR_PSW.x, 7, C & B)
+    p += f"ANL C, {atl.BIT(addr, idx)}"
+
+    p += atl.aste(SFR_PSW, atl.I(ram[SFR_PSW.x]))
+    return ""
+
+for x in range(29):
+    p.iter_is(init)
+    p.iter_bit(test)
+
