50c6dddfd07e183872eed3641e36d0bd2de9a8be
Aimini <2026084033@qq.com>
2020-01-25 19:17:20 +0800

    feat: 0xB2, CPL bit. test passed.

---
+++ test/generate/B2_CPL_b.py
@@ -0,0 +1,32 @@
+#########################################################
+# 2020-01-25 16:54:33
+# AI
+# ins: CPL bit
+#########################################################
+
+import __util as u
+import random
+from __asmconst import *
+from __numutil import numutil as ntl
+from __51util import SIMRAM
+
+p = u.create_test()
+ram = SIMRAM()
+
+
+def init(addr, p):
+    v = random.getrandbits(8)
+    p += atl.move(atl.D(addr),atl.I(v))
+    ram[addr] = v
+    return ""
+
+def test(addr, idx, p):
+    ram.set_bit(addr,idx, ~ram.bit(addr,idx))
+    p += f"CPL {atl.BIT(addr, idx)}"
+
+    p += atl.aste(atl.D(addr), atl.I(ram[addr]))
+    return ""
+for x in range(29):
+    p.iter_is(init)
+    p.iter_bit(test)
+
