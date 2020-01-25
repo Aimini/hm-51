23faee87f6e13c02f1fed16a7a5840a99c2e0938
Aimini <2026084033@qq.com>
2020-01-25 19:30:37 +0800

    feat: 0xA2, MOV C, bit. test passed.

---
+++ test/generate/A2_MOV_C_b.py
@@ -0,0 +1,35 @@
+#########################################################
+# 2020-01-25 17:39:15
+# AI
+# ins: ORL C, bit
+#########################################################
+
+import __util as u
+import random
+from __asmconst import *
+from __numutil import numutil as ntl
+from __51util import SIMRAM
+
+p = u.create_test()
+ram = SIMRAM()
+
+
+def init(addr, p):
+    v = random.getrandbits(8)
+    p += atl.move(atl.D(addr),atl.I(v))
+    ram[addr] = v
+    return ""
+
+def test(addr, idx, p):
+    ram.set_bit(SFR_PSW.x, 7, ram.bit(addr,idx))
+    
+    p += f"MOV C, {atl.BIT(addr, idx)}"
+
+    p += atl.aste(SFR_PSW, atl.I(ram[SFR_PSW.x]))
+    return ""
+
+for x in range(29):
+    p.iter_is(init)
+    p.iter_bit(test)
+    p += atl.dump()
+
