6da224491a5f9771c9e4cc50f97a09509622df0c
Aimini <2026084033@qq.com>
2020-01-25 16:23:06 +0800

    feat: 0xC2, CLR bit. test passed.

---
+++ test/generate/C2_CLR_b.py
@@ -0,0 +1,32 @@
+#########################################################
+# 2020-01-25 16:20:20
+# AI
+# ins: CLR bit
+#########################################################
+
+import __util as u
+import random
+from __asmconst import *
+from __numutil import numutil as ntl
+from __51util import SIMRAM
+
+p = u.create_test()
+ram = SIMRAM()
+
+
+def init(addr, p):
+    v = random.getrandbits(8)
+    p += atl.move(atl.D(addr),atl.I(v))
+    ram[addr] = v
+    return ""
+
+def test(addr, idx, p):
+    ram.set_bit(addr,idx,0)
+    p += f"CLR {atl.BIT(addr, idx)}"
+
+    p += atl.aste(atl.D(addr), atl.I(ram[addr]))
+    return ""
+for x in range(29):
+    p.iter_is(init)
+    p.iter_bit(test)
+
