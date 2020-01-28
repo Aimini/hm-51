8d28c076f422d0b1c740b9f98af1dc490f295e2a
Aimini <2026084033@qq.com>
2020-01-28 12:05:08 +0800

    feat: 0xE5, MOV direct, A. test passed.

---
+++ test/generate/E5_MOV_A_d.py
@@ -0,0 +1,30 @@
+#########################################################
+# 2020-01-28 12:03:11
+# AI
+# ins: MOV A, direct
+#########################################################
+
+import random
+import __util as u
+from __asmconst import *
+from __51util import SIMRAM
+
+p = u.create_test()
+ram = SIMRAM()
+
+def fill_one(addr, p):
+    value = random.getrandbits(8)
+    p += f'''
+    MOV {atl.D(addr)}, {atl.I(value)}
+    MOV A, {atl.D(addr)}
+    '''
+    ram[addr] = value
+    ram[SFR_A.x] = ram[addr]
+    p += atl.aste(SFR_A, atl.I(ram[SFR_A.x]))
+    return ""
+
+def one():
+    p.iter_is(fill_one)
+
+for _ in range(32):
+    one()
