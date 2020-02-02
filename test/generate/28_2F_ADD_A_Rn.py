73fdd6dc78d6bcf0c9ace1579dc76c178212796c
Aimini <2026084033@qq.com>
2020-02-02 14:54:56 +0800

    refactor: rewrite and test 0x28-0x2F, ADD A, Rn.

--- test/generate/28_2F_ADD_A_Rn.py
+++ test/generate/28_2F_ADD_A_Rn.py
@@ -1,7 +1,7 @@
 #########################################################
 # 2020-01-23 16:03:11
 # AI
-# ins: ORL A, @Rn
+# ins: ADDC A, Rn
 #########################################################
 
 import __util as u
@@ -10,90 +10,96 @@ from __asmconst import *
 from __numutil import numutil as ntl
 from __51util import SIMRAM
 
-p = u.create_test()
-ram = SIMRAM()
 
-op_name = "ADD"
+class XXX_R_RN:
+    def __init__(self, op_name):
+        self.ram = SIMRAM()
+        self.p = u.create_test()
+        self.op_name = op_name
+
+    def op_func(self, B):
+        pass
+
+    def gen(self, iter_test_time, random_test_time):
+        ram = self.ram
+        p = self.p
+        op_func = self.op_func
+        op_name = self.op_name
+
+        def empty_rs(rs, psw_rs, p):
+            pass
+
+        def init_rn(RN, p):
+            p += f';;  load random value into {RN}'
+            value = random.getrandbits(8)
+
+            p += atl.move(atl.D(RN.addr), atl.I(value))
+            ram.set_iram(RN.addr, value)
+
+        def test_rs(rs, psw_rs, p):
+            a = 0x00
+            p += atl.move(SFR_PSW, atl.I(psw_rs))
+            p += atl.move(SFR_A, atl.I(a))
+
+            ram.set_direct(SFR_PSW.x, psw_rs)
+            ram.set_direct(SFR_A.x, a)
+
+        def test_rn(RN, p):
+            p += f'{self.op_name} A, {RN}'
+            op_func(ram.get_iram(RN.addr))
+            p += atl.aste(SFR_A, atl.I(ram.get_direct(SFR_A.x)))
+        
+        def test_bound(RN, p):
+            A = random.getrandbits(8)
+            p += atl.move(SFR_PSW.x, atl.I(RN.psw_rs))
+            ram.set_direct(SFR_PSW.x, RN.psw_rs)
+            p += atl.move(SFR_A, atl.I(A))
+            ram.set_direct(SFR_A.x, A)
+
+            for value in ntl.bound(8):
+                p += atl.move(atl.D(RN.addr),  atl.I(value))
+                ram.set_iram(RN.addr, value)
+
+                p += f'{self.op_name} A, {RN}'
+                op_func(ram.get_iram(RN.addr))
+
+                p += atl.aste(SFR_A, atl.I(ram.get_direct(SFR_A.x)))
+            
+            
+        for x in range(iter_test_time):
+            p.iter_rn(empty_rs, init_rn)
+            p.iter_rn(test_rs,  test_rn)
+
+        for x in range(random_test_time):
+            p.iter_rn(empty_rs, test_bound)
 
 
-def do(B):
-    cy = 0 #(ram[SFR_PSW.x] & 0x80) >> 7
-    A = ram[SFR_A.x]
-    SL = (A&0xF) + (B&0xF) + cy
-    S  =  A  + B + cy
+class ADD_A_Rn(XXX_R_RN):
+    def __init__(self, op_name, use_psw_cy):
+        super().__init__(op_name)
+        self.use_psw_cy = use_psw_cy
 
-    if S > 0xFF:
-        ram[SFR_PSW.x] |= 0x80
-    else:
-        ram[SFR_PSW.x] &= 0x7F
+    def op_func(self, B):
+        if self.use_psw_cy:
+            cy = self.ram.get_bit(SFR_PSW.x, 7)
+        else:
+            ci = 0
+        A = self.ram.get_direct(SFR_A.x)
+
+        SL = (A & 0xF) + (B & 0xF) + ci
+        S = A + B + ci
+
+        cy = 1 if S > 0xFF else 0
+        self.ram.set_bit(SFR_PSW.x, 7, cy)
+
+        ac = 1 if SL > 0xF else 0
+        self.ram.set_bit(SFR_PSW.x, 6, ac)
+
+        ov = 1 if((~(A ^ B)) & (A ^ S)) & 0x80 else 0
+        self.ram.set_bit(SFR_PSW.x, 2, ov)
+
+        self.ram.set_direct(SFR_A.x, S & 0xFF)
     
-    if SL > 0xF:
-        ram[SFR_PSW.x] |= 0x40
-    else:
-        ram[SFR_PSW.x] &= 0xBF
-
-    if ((~(A ^ B)) & (A ^ S)) & 0x80:
-        ram[SFR_PSW.x] |= 0x04
-    else:
-        ram[SFR_PSW.x] &= 0xFB
-    ram[SFR_A.x] = S & 0xFF
-
-
-
-
-
-def init_rs(rs, psw_rs, p):
-    p += f";;  set register bank{rs} in psw" 
-
-    p += atl.move(SFR_PSW, atl.I(psw_rs))
-    ram[SFR_PSW.x] = psw_rs
-
-    
-
-
-def init_rn(RN, p):
-    p += f";;  load random value into {RN}" 
-
-    value = random.getrandbits(8)
-
-    p += atl.move(atl.D(RN.addr), atl.I(value))
-    ram[RN.addr] = value
-
-    
-
-
-def test_rs(rs, psw_rs, p):    
-    a = 0x00
-    p += atl.move(SFR_PSW, atl.I(psw_rs))
-    p += atl.move(SFR_A, atl.I(a))
-
-    ram[SFR_PSW.x] = psw_rs
-    ram[SFR_A.x] = a
-    
-
-def test_rn(RN, p):
-    p += f"{op_name} A, {RN}"
-    do(ram[RN.addr])
-
-    p += atl.aste(SFR_A, atl.I(ram[SFR_A.x]))
-    
-
-
-def test_bound(RN, p):
-    A = random.getrandbits(8)
-    p += atl.move(SFR_A, atl.I(A))
-    ram[SFR_A.x] = A
-    for value in ntl.bound(8):
-        p += atl.move(atl.D(RN.addr),  atl.I(value))
-        p += f"{op_name} A, {RN}"
-
-        do(ram[RN.addr])
-        ram[RN.addr] = value
-    
-
-
-for x in range(128):
-    p.iter_rn(init_rs, init_rn)
-    p.iter_rn(test_rs, test_rn)
-
-p.iter_rn(lambda a,b: "", test_bound)
+if __name__ == '__main__':
+    tt = ADD_A_Rn("ADD", False)
+    tt.gen(128, 1)
