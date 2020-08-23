9c27c44c39b34af22339d23a07aee7fba012b057
Aimini <2026084033@qq.com>
2020-08-23 19:45:25 +0800

    fix: alus.py SELHIRQ function

--- src/alu/alus.py
+++ src/alu/alus.py
@@ -41,7 +41,7 @@ def generate_by_op(ci, f, A):
         R = (A & 0xB7) | ((A & 0x08) << 3) | ((A & 0x40) >>3)
     elif f == 0x1: #IVADDR
         # don't forget remove useless flag
-        IRNQ = bin(A & 0x7F)[::-1].find('1')
+        IRNQ = (A & -A).bit_length() - 1
         R = (IRNQ << 3) + 3
     elif f == 0x2: #CAA
         if ci:
@@ -88,12 +88,12 @@ def generate_by_op(ci, f, A):
         IR_H = ((A >> 4) & 3)
         if IV_L:
             if IV_H and IP_H and not IP_L:
-                R = ((IR_H + 4) << 1) | (IP_H << 7)
+                R = (1 << (IR_H + 4)) | (IP_H << 7)
             else:
-                R = (IR_L << 1) | (IP_L << 7)
+                R = (1 << IR_L) | (IP_L << 7)
         else:
             if IV_H:
-                R = ((IR_H + 4) << 1) | (IP_H << 7)
+                R = (1 << (IR_H + 4)) | (IP_H << 7)
 
     elif f == 0xE:  # ISRRET ISR
         if A & 0x40:
