18856992b11768cf66ca378669b095474e51ed3d
Aimini <2026084033@qq.com>
2020-11-12 15:06:44 +0800

    feat: add tool to help you split docoder file

---
+++ tools/splitbytebin.py
@@ -0,0 +1,47 @@
+#
+# Author AI
+# splite binary file with N bytes per address 
+# to indivaul binary files that consist with one byte per address 
+
+
+import sys
+import pathlib
+usage = """
+ splitbytebin.py <input_file> <n>
+    Treat input_file as a file containing <n> bytes per line,
+    the <n>th bytes in each line are stored in a separate file. 
+    The output file name is the original file name followed by <n>.
+
+    inpu_file:
+        input file name
+    n: int
+        how many byte per line
+
+    for example
+
+        input.txt
+            ABCDEF
+
+        spiltbytebin.py input.txt 3
+
+        input.txt1
+            AB
+        input.txt2
+            CD    
+        input.txt3
+            EF
+"""
+
+if len(sys.argv) < 3:
+    print(usage)
+else:
+    infile = pathlib.Path(sys.argv[1])
+    n = int(sys.argv[2])
+    with open(infile,'rb') as fih:
+        data = fih.read()
+        sdata = [data[x:x+n] for x in range(0,len(data),n)]
+        zdata = zip(*sdata) 
+        for i,od in enumerate(zdata):
+            
+            with open( infile.parent /(infile.stem + str(i) + infile.suffix),'wb') as foh:
+                foh.write(bytearray(od))
