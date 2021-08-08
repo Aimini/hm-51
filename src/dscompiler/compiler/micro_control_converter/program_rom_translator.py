28cf1f4a77f2354bfabc48ac96c1463a0e6e446b
Aimini <2026084033@qq.com>
2021-08-08 14:20:34 +0800

    fix:  function of programming ROM

--- src/dscompiler/compiler/micro_control_converter/program_rom_translator.py
+++ src/dscompiler/compiler/micro_control_converter/program_rom_translator.py
@@ -1,5 +1,5 @@
 
-from .empty_translator import empty_translator
+from .empty_translator import TranlatorError, empty_translator
 
 class PROMTranslator(empty_translator):
     def translate(self, dt):
@@ -10,15 +10,18 @@ class PROMTranslator(empty_translator):
         r = []
         # ACTIVE, ~CE, WE is reused from IMMED[0:2]
         # ACTIVE bit will be set by default
-        immed = 0x3 # buffer's direction is always from data bus to ROM
+        immed = 0x2 # buffer's direction is always from data bus to ROM
                      # ~CE deafults to high(deactive)
-                     # WE defaults to low
+                     # WE defaults to low(deactive)
         for p in dt.parameters:
-            if p == "CE":
-                immed ^= 0x2
+            if p == "ACTIVE":
+                immed |= 0x1
+            elif p == "CE":
+                immed &= 0xD
             elif p == "WE":
                 immed |= 0x4
-                
+            else:
+                raise TranlatorError(f"unkown token \"{p}\"")
         r.append(self.create_immed(dt.lineno, immed))
         r.append(self.create_loadprr(dt.lineno))
         return r
